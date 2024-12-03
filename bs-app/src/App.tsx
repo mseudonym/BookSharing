92d4fee115d0090cb06b1ece61d77523fdbef1fb
dmitrialekhin <dmitrialekhin@ozon.ru>
2024-12-03 15:33:29 +0300

    add page header

--- bs-app/src/App.tsx
+++ bs-app/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import avatar from './assets/UserImg.png'; // Путь к аватару
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    return (
+        <div className="App">
+            <div className="profile-header">
+                <div className="profile-avatar">
+                    <img src={avatar} alt="Avatar" className="avatar-image" />
+                </div>
+                <div className="profile-info">
+                    <p className="profile-name">Александр</p>
+                    <p className="profile-surname">Михайличенко</p>
+                    <p className="profile-username">@mseudonym</p>
+                    <p className="profile-contact">Связаться</p>
+                </div>
+            </div>
+            <div className="content-space">
+            </div>
+        </div>
+    );
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
