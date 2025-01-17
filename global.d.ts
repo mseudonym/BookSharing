8d259273b514365733149eb35149316f4c341793
Михайличенко Александр <meadow174@gmail.com>
2025-01-17 11:24:37 +0500

    fix global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 declare module '*.module.css' {
     const classes: { [key: string]: string };
     export default classes;
-=======
-declare module '*.module.css' {
-    const classes: { [key: string]: string };
-    export default classes;
->>>>>>> refs/remotes/origin/main
 }
