5720a4e7ac115bd4737c50ebd9b03d67f86cefa0
Михайличенко Александр <meadow174@gmail.com>
2025-01-19 20:35:29 +0500

    added api in queue

--- src/components/queue/queue.tsx
+++ src/components/queue/queue.tsx
@@ -2,62 +2,60 @@ import _styles from '../../index.module.css'
 import styles from './queue.module.css';
 import { Avatar, Flex } from '@mantine/core';
 import { Button } from '../buttons/button';
+import { Link } from 'react-router';
+import { FC } from 'react';
+import { QueueModel } from '../../generated-api/model';
 
-export interface QueueProps {
-  owner: string;
-  currentHolder: string;
-  queueAvatars: string[]
-}
+export const Queue: FC<QueueModel> = ({ owner, holder, queue }) => {
+  if (queue == undefined || owner == undefined) {
+    return <p>Нет очередей</p>
+  }
 
-export const Queue: React.FC<QueueProps> = ({
-  owner,
-  currentHolder,
-  queueAvatars
-}) => {
   return (
     <article className={styles.queue}>
-      <Flex gap={8} direction="column">
+      <div className={styles.personWrapper}>
         <span className={_styles.textGray}>Владелец</span>
-        <Flex gap={16}>
-          <Avatar src='src/assets/default-profile.png'
+        <div className={styles.person}>
+          <Avatar src={owner.lowQualityPhotoUrl ?? "/src/assets/default-profile.png"}
             radius='xl'
             size={41} />
-          <Flex gap={4} direction="column">
-            <p>{owner}</p>
-            <a className={_styles.link}>Перейти в профиль</a>
-          </Flex>
-        </Flex>
-      </Flex>
-
-      <Flex gap={8} direction="column">
-        <span className={_styles.textGray}>Текущий держатель</span>
-        <Flex gap={16}>
-          <Avatar src='src/assets/default-profile.png'
-            radius='xl'
-            size={41} />
-          <Flex gap={4} direction="column">
-            <p>{currentHolder}</p>
-            <a className={_styles.link}>Связаться</a>
-          </Flex>
-        </Flex>
-      </Flex>
-
-      <Flex gap={4} direction="column">
-        <span className={_styles.textGray}>{queueAvatars.length} человек в очереди</span>
-        <div className={styles.avatarStack}>
-          {queueAvatars.map((avatar, index) => (
-            <Avatar
-              key={index}
-              src={avatar}
-              size={41}
-              radius="xl"
-              className={styles.avatar}
-              style={{zIndex: index}}
-            />
-          ))}
+          <div className={styles.personInfo}>
+            <p className={styles.name}>{owner.username}</p>
+            <Link className={_styles.link} to={`/user/${owner.id}`}>Перейти в профиль</Link>
+          </div>
         </div>
-      </Flex>
+      </div>
 
+      <div className={styles.personWrapper}>
+        <span className={_styles.textGray}>Текущий держатель</span>
+        {holder == undefined ? <p>Пока никого</p> :
+          <div className={styles.person}>
+            <Avatar src={holder.lowQualityPhotoUrl ?? "/src/assets/default-profile.png"}
+              radius='xl'
+              size={41} />
+            <div className={styles.personInfo}>
+              <p className={styles.name}>{holder.username}</p>
+              <Link className={_styles.link} to={`/user/${holder.id}`}>Связаться</Link>
+            </div>
+          </div>
+        }
+      </div>
+
+      {queue == undefined || queue.length == 0 ? <p className={_styles.textGray}>Пока что никого нет в очереди, но вы можете быть первым.</p> :
+        <Flex gap={4} direction="column">
+          <span className={_styles.textGray}>{queue.length} человек в очереди</span>
+          <Avatar.Group>
+            {queue.map((avatar, index) => (
+              <Avatar
+                key={index}
+                src={avatar.lowQualityPhotoUrl}
+                size={41}
+                radius="xl"
+              />
+            ))}
+          </Avatar.Group>
+        </Flex>
+      }
       <Button variant='fill'>Встать в очередь</Button>
     </article>
   );
