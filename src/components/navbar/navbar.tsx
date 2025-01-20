08cf34dc3853687d2adb4acac706f6d3263faa2b
Михайличенко Александр <meadow174@gmail.com>
2025-01-20 10:46:13 +0500

    tried to fix navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,16 +3,20 @@ import { SegmentedControl } from '@mantine/core';
 import { AppRoute } from '../../conts';
 import { BooksLibraryIcon24Regular, People3Icon24Regular, FolderIcon24Regular, People1Icon24Regular } from "@skbkontur/icons";
 import { useNavigate } from 'react-router';
+import { useState } from 'react';
 
 export const Navbar = () => {
   const navigate = useNavigate();
+  
+  const [activePage, setActivePage] = useState('');
 
   const handleChange = (value: string) => {
+    setActivePage(value);
     navigate(value);
   }
 
   return (
-    <SegmentedControl size='md' withItemsBorders={false} radius='xl' onChange={handleChange} data={[
+    <SegmentedControl size='md' withItemsBorders={false} radius='xl' value={activePage} onChange={handleChange} data={[
       {
         value: `${AppRoute.Shelf}`,
         label: (
@@ -40,7 +44,7 @@ export const Navbar = () => {
     ]} classNames={{
       root: `${styles.navbarRoot}`,
       label: `${styles.navbarLabel}`,
-      indicator: `${styles.navbarIndicator}`,
+      //indicator: `${styles.navbarIndicator}`,
     }}
     />
   );
