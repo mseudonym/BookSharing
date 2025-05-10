983af3afcdbcb6f3d9a0884c6c2da77aa309fdf8
Михайличенко Александр <meadow174@gmail.com>
2025-05-10 17:17:21 +0500

    tried to fix navbar

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -1,6 +1,4 @@
-import styles from './navbar.module.css';
-import { Tabs } from '@mantine/core';
-import { AppRoute } from '../../conts';
+import { SegmentedControl } from '@mantine/core';
 import {
   BooksLibraryIcon24Regular,
   BooksLibraryIcon24Solid,
@@ -9,44 +7,59 @@ import {
   FolderIcon24Regular,
   FolderIcon24Solid,
   People1Icon24Regular,
-  People1Icon24Solid,
+  People1Icon24Solid
 } from '@skbkontur/icons';
-import { useLocation, useNavigate } from 'react-router';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { AppRoute } from '../../conts';
+import styles from './navbar.module.css';
 
 export const Navbar = () => {
   const navigate = useNavigate();
-  const location = useLocation();
+  const { pathname } = useLocation();
 
-  const handleChange = (value: string | null) => {
-    if (value) {
-      navigate(value);
-    }
+  const navItems = [
+    {
+      value: AppRoute.Shelf,
+      iconRegular: <BooksLibraryIcon24Regular />,
+      iconSolid: <BooksLibraryIcon24Solid />,
+    },
+    {
+      value: AppRoute.Friends,
+      iconRegular: <People2Icon24Regular />,
+      iconSolid: <People2Icon24Solid />,
+    },
+    {
+      value: AppRoute.Storage,
+      iconRegular: <FolderIcon24Regular />,
+      iconSolid: <FolderIcon24Solid />,
+    },
+    {
+      value: AppRoute.Profile,
+      iconRegular: <People1Icon24Regular />,
+      iconSolid: <People1Icon24Solid />,
+    },
+  ];
+
+  const handleNavigate = (value: string) => {
+    navigate(value);
   };
 
   return (
-    <Tabs
-      value={location.pathname}
-      onChange={handleChange}
+    <SegmentedControl
+      size="md"
+      withItemsBorders={false}
+      radius="xl"
+      value={pathname}
+      onChange={handleNavigate}
+      data={navItems.map((item) => ({
+        value: item.value,
+        label: pathname === item.value ? item.iconSolid : item.iconRegular,
+      }))}
       classNames={{
         root: styles.navbarRoot,
-        list: styles.navbarList,
-        tab: styles.navbarTab,
+        label: styles.navbarLabel,
+        indicator: styles.navbarIndicator,
       }}
-    >
-      <Tabs.List>
-        <Tabs.Tab value={AppRoute.Shelf}>
-          {location.pathname === AppRoute.Shelf ? <BooksLibraryIcon24Solid /> : <BooksLibraryIcon24Regular />}
-        </Tabs.Tab>
-        <Tabs.Tab value={AppRoute.Friends}>
-          {location.pathname === AppRoute.Friends ? <People2Icon24Solid /> : <People2Icon24Regular />}
-        </Tabs.Tab>
-        <Tabs.Tab value={AppRoute.Storage}>
-          {location.pathname === AppRoute.Storage ? <FolderIcon24Solid /> : <FolderIcon24Regular />}
-        </Tabs.Tab>
-        <Tabs.Tab value={AppRoute.Profile}>
-          {location.pathname === AppRoute.Profile ? <People1Icon24Solid /> : <People1Icon24Regular />}
-        </Tabs.Tab>
-      </Tabs.List>
-    </Tabs>
+    />
   );
 };
