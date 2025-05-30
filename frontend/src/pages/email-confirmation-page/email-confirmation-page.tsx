88c5bd1585a84698fe70062610e6e4b4367e203a
Михайличенко Александр <meadow174@gmail.com>
2025-05-30 16:12:59 +0500

    fix: Email confirm

--- frontend/src/pages/email-confirmation-page/email-confirmation-page.tsx
+++ frontend/src/pages/email-confirmation-page/email-confirmation-page.tsx
@@ -1,54 +1,57 @@
-import {Text, Title} from '@mantine/core';
-import React, {useEffect} from 'react';
+import { Text, Title } from '@mantine/core';
+import { notifications } from '@mantine/notifications';
+import { useMutation } from '@tanstack/react-query';
+import React, { useEffect } from 'react';
+import { useSearchParams } from 'react-router';
+
 import _styles from '~/index.module.css';
-import {Header} from '~/components/header';
-import {PageWithWrapper} from '~/ui/pages';
-import {useSearchParams} from 'react-router';
-import {useMutation} from "@tanstack/react-query";
-import {postAuthConfirmEmail} from "~/generated-api/auth/auth";
-import {notifications} from "@mantine/notifications";
+
+import { Header } from '~/components/header';
+import { postAuthConfirmEmail } from '~/generated-api/auth/auth';
+import { PageWithWrapper } from '~/ui/pages';
+
 
 export const EmailConfirmationPage = () => {
-    const [searchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
 
-    const userId = searchParams.get('userId') || undefined;
-    const code = searchParams.get('code') || undefined;
+  const userId = searchParams.get('userId') || undefined;
+  const code = searchParams.get('code') || undefined;
     
-    // Если прилетел email — значит пользователь изменил почту, нужно отобразить это в интерфейсе
-    const email = searchParams.get('email') || undefined;
+  // Если прилетел email — значит пользователь изменил почту, нужно отобразить это в интерфейсе
+  const email = searchParams.get('email') || undefined;
 
-    const { isPending, isError, mutateAsync: confirmEmailMutation } = useMutation({
-        mutationFn: postAuthConfirmEmail,
-        onError: (error) => {
-            notifications.show({
-                title: 'Ошибка подтверждения',
-                message: 'Не удалось подтвердить email',
-                color: 'red',
-            });
-        },
-    });
+  const { isPending, isError, mutateAsync: confirmEmailMutation } = useMutation({
+    mutationFn: postAuthConfirmEmail,
+    onError: (error) => {
+      notifications.show({
+        title: 'Ошибка подтверждения',
+        message: 'Не удалось подтвердить email',
+        color: 'red',
+      });
+    },
+  });
 
-    useEffect(() => {
-        if (userId && code) {
-            confirmEmailMutation({userId, code, changedEmail: email}).then();
-        }
-    }, [userId, code, email]);
+  useEffect(() => {
+    if (userId && code) {
+      confirmEmailMutation({ userId, code, changedEmail: email }).then();
+    }
+  }, [userId, code, email]);
 
-    return (
-        <PageWithWrapper alignWrapper="center" withoutMenu>
-            <Title ta='center'>
+  return (
+    <PageWithWrapper alignWrapper="center" withoutMenu>
+      <Title ta='center'>
                 Подтверждение почты
-            </Title>
-            <Text ta='center' className={_styles.textGray}>
+      </Title>
+      <Text ta='center' className={_styles.textGray}>
                 Чтобы это сделать, перейдите по ссылке, отправленной на почту.
-            </Text>
-            <Header variant="left">
-                <Title order={2}>Подтверждение почты</Title>
-            </Header>
+      </Text>
+      <Header variant="left">
+        <Title order={2}>Подтверждение почты</Title>
+      </Header>
 
-            {isPending && <p>Подтверждение...</p>}
-            {isError && <p>Не удалось подтвердить почту, ссылка устарела</p>}
-            {!isPending && !isError && <p>Email успешно подтвержден!</p>}
-        </PageWithWrapper>
-    );
+      {isPending && <p>Подтверждение...</p>}
+      {isError && <p>Не удалось подтвердить почту, ссылка устарела</p>}
+      {!isPending && !isError && <p>Email успешно подтвержден!</p>}
+    </PageWithWrapper>
+  );
 };
--- frontend/src/pages/email-confirmation-waiting-page/email-confirmation-waiting-page.tsx
+++ frontend/src/pages/email-confirmation-waiting-page/email-confirmation-waiting-page.tsx
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from 'react';
 
 import _styles from '~/index.module.css';
 
-import { checkProfileFilling, redirectIfAuth } from '~/actions/user-actions';
+import { checkProfileFilling } from '~/actions/user-actions';
 import { Header } from '~/components/header';
 import { IllustrationWrapper } from '~/components/illustration-wrapper';
 import { RESEND_CONFIRMATION_EMAIL_SECONDS } from '~/conts';
@@ -14,70 +14,66 @@ import { getGetUsersMeQueryKey, getUsersMe } from '~/generated-api/users/users';
 import { PageWithWrapper } from '~/ui/pages';
 
 export const EmailConfirmationWaitingPage = () => {
-    useEffect(() => {
-        redirectIfAuth();
-    }, []);
+  const [seconds, setSeconds] = useState<number>(RESEND_CONFIRMATION_EMAIL_SECONDS);
 
-    const [seconds, setSeconds] = useState<number>(RESEND_CONFIRMATION_EMAIL_SECONDS);
+  const { mutateAsync: resendConfirmationEmail } = useMutation({
+    mutationFn: postAuthResendConfirmationEmail,
+    onMutate: async () => {
+      setSeconds(RESEND_CONFIRMATION_EMAIL_SECONDS);
+    },
+  });
 
-    const { mutateAsync: resendConfirmationEmail } = useMutation({
-        mutationFn: postAuthResendConfirmationEmail,
-        onMutate: async () => {
-            setSeconds(RESEND_CONFIRMATION_EMAIL_SECONDS);
-        },
-    });
+  useEffect(() => {
+    const timeInterval = setTimeout(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      }
+    }, 1000);
+    return () => {
+      clearInterval(timeInterval);
+    };
+  });
 
-    useEffect(() => {
-        const timeInterval = setTimeout(() => {
-            if (seconds > 0) {
-                setSeconds(seconds - 1);
-            }
-        }, 1000);
-        return () => {
-            clearInterval(timeInterval);
-        };
-    });
+  const { data: userData } = useQuery({
+    queryFn: () => getUsersMe(),
+    queryKey: getGetUsersMeQueryKey(),
+    refetchInterval: 6000,
+  });
 
-    const { data: userData } = useQuery({
-        queryFn: () => getUsersMe(),
-        queryKey: getGetUsersMeQueryKey(),
-        refetchInterval: 6000,
-    });
+  useEffect(() => {
+    if (userData?.isEmailConfirm) {
+      checkProfileFilling(userData, true);
+    }
+  }, [userData]);
 
-    useEffect(() => {
-        if (userData?.isEmailConfirm) {
-            checkProfileFilling(userData, true);
-        }
-    }, [userData]);
-
-    return (
-        <PageWithWrapper alignWrapper="center" withoutMenu>
-            <Header variant="left">
-                <ActionIcon variant="transparent" onClick={() => { window.history.back(); }}>
-                    <ArrowALeftIcon24Regular />
-                </ActionIcon>
-            </Header>
-            <Title ta='center'>
+  return (
+    <PageWithWrapper alignWrapper="center" withoutMenu>
+      <Header variant="left">
+        <ActionIcon variant="transparent" onClick={() => { window.history.back(); }}>
+          <ArrowALeftIcon24Regular />
+        </ActionIcon>
+      </Header>
+      <Title ta='center'>
                 Ожидаем подтверждение почты
-            </Title>
-            <Text ta='center' className={_styles.textGray}>
+      </Title>
+      <Text ta='center' className={_styles.textGray}>
                 Чтобы это сделать, перейдите по ссылке, отправленной на почту.
-            </Text>
-            <IllustrationWrapper
-                src="/mail.svg"
-                alt="Email confirmation illustration"
-            />
-            {seconds === 0
-                ? (
-                    <Anchor className={_styles.anchorGray} onClick={() => resendConfirmationEmail({ email: userData?.email ?? '' })}>
+      </Text>
+      <IllustrationWrapper
+        src="/mail.svg"
+        alt="Email confirmation illustration"
+      />
+      {seconds === 0
+        ? (
+          <Anchor className={_styles.anchorGray} onClick={() => resendConfirmationEmail({ email: userData?.email ?? '' })}>
                         Отправить письмо ещё раз
-                    </Anchor>
-                )
-                : (
-                    <Text ta='center' className={_styles.textGray}>
-                        {`Отправить письмо повторно можно через ${seconds} секунд.`}
-                    </Text>
-                )}
-        </PageWithWrapper>
-    );
+          </Anchor>
+        )
+        : (
+          <Text ta='center' className={_styles.textGray}>
+            {`Отправить письмо повторно можно через ${seconds} секунд.`}
+          </Text>
+        )}
+    </PageWithWrapper>
+  );
 };
