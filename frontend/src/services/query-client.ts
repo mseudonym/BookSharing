542e40b992eab9cf6e58d330604137be0d5f81e4
Михайличенко Александр <meadow174@gmail.com>
2025-06-13 17:37:36 +0500

    fix: Fixed node version in docker

--- frontend/src/services/query-client.ts
+++ frontend/src/services/query-client.ts
@@ -1,16 +1,12 @@
 import { QueryClient } from '@tanstack/react-query';
-
+import { AxiosError } from 'axios';
 
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      retry: (failureCount, error: any) => {
-        // Check if the error is a 404 (you may need to adjust based on your error structure)
-        if (error?.response?.status === 404) {
-          return false; // Don't retry on 404
-        }
-        // Retry up to 3 times for other errors
-        return failureCount < 3;
+      retry: (_, error) => {
+        const axiosError = error as AxiosError;
+        return axiosError.response?.status !== 404;
       },
     },
   },
